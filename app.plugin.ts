19df2393f0613fb30c757c5f539264f194e82fe9
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2023-08-17 13:16:39 +0100

    fix: silent error on android build

--- app.plugin.ts
+++ app.plugin.ts
@@ -12,38 +12,34 @@ type Params = {
 
 function withIconBadge(config: any, { badges, enabled = true }: Params) {
   if (!enabled) return config;
+  // get source paths from config
+  const iconPath = config?.icon;
+  const adaptiveIconPath = config?.android?.adaptiveIcon?.foregroundImage;
+  // TODO: add more checks for the config object
 
-  try {
-    // get source paths from config
-    const iconPath = config?.icon;
-    const adaptiveIconPath = config?.android?.adaptiveIcon?.foregroundImage;
-    // TODO: add more checks for the config object
+  // Generate icon with badge
+  // normally addBadge is async but we don't need to wait for it also not sure how to use async in this context
+  if (iconPath) {
+    addBadge({
+      icon: iconPath,
+      dstPath: DST_ICON,
+      badges,
+    }).catch(() => {}); // we silently fail to prevent error in android build process
 
-    // Generate icon with badge
-    // normally addBadge is async but we don't need to wait for it also not sure how to use async in this context
-    if (iconPath) {
-      addBadge({
-        icon: iconPath,
-        dstPath: DST_ICON,
-        badges,
-      });
-      config.icon = DST_ICON;
-    }
-
-    // waiting for the adaptive icon support here
-    if (adaptiveIconPath) {
-      addBadge({
-        isAdaptiveIcon: true,
-        icon: adaptiveIconPath,
-        dstPath: DST_ADAPTIVE_APP_ICON,
-        badges,
-      });
-      config.android.adaptiveIcon.foregroundImage = DST_ADAPTIVE_APP_ICON;
-    }
-
-    return config;
-  } catch (error) {
-    return config;
+    config.icon = DST_ICON;
   }
+
+  // waiting for the adaptive icon support here
+  if (adaptiveIconPath) {
+    addBadge({
+      isAdaptiveIcon: true,
+      icon: adaptiveIconPath,
+      dstPath: DST_ADAPTIVE_APP_ICON,
+      badges,
+    }).catch(() => {});
+    config.android.adaptiveIcon.foregroundImage = DST_ADAPTIVE_APP_ICON;
+  }
+
+  return config;
 }
 module.exports = withIconBadge;

ee70c9d4a3c57c167be92e07c29a610cbe0471b6
janwiebe-jump <64576907+janwiebe-jump@users.noreply.github.com>
2024-09-03 07:45:38 +0000

    fix(plugin): refactor to use withDangerousMod

--- app.plugin.ts
+++ app.plugin.ts
@@ -1,8 +1,10 @@
 import { addBadge } from './index';
 import { Badge } from './types';
+import { withDangerousMod } from '@expo/config-plugins'
 
 const DST_APP_ICON_BADGE_FOLDER = '.expo/app-icon-badge';
 const DST_ICON = `${DST_APP_ICON_BADGE_FOLDER}/icon.png`;
+const DST_IOS_ICON = `${DST_APP_ICON_BADGE_FOLDER}/ios-icon.png`;
 const DST_ADAPTIVE_APP_ICON = `${DST_APP_ICON_BADGE_FOLDER}/foregroundImage.png`;
 
 type Params = {
@@ -10,8 +12,87 @@ type Params = {
   enabled?: boolean;
 };
 
+
+async function withIcon(config: any, badges: Array<Badge>) {
+  
+  const iconPath = config?.icon;
+  
+  if (iconPath) {
+    await addBadge({
+      icon: iconPath,
+      dstPath: DST_ICON,
+      badges,
+    }).catch(() => {}); // we silently fail to prevent error in android build process
+
+    config.icon = DST_ICON;
+  }
+  return config
+}
+
+function withIconBadgeiOS(config: any, badges: Array<Badge>) {
+  return withDangerousMod(config, [
+    "ios",
+    async (config) => {
+
+      // Update the main icon
+      config = await withIcon(config, badges)
+
+      const iconPath = config?.ios?.icon;
+  
+      if (iconPath) {
+        await addBadge({
+          icon: iconPath,
+          dstPath: DST_ICON,
+          badges,
+        }).catch(() => {}); // we silently fail to prevent error in android build process
+    
+        config!.ios!.icon = DST_ICON;
+      }
+
+      return config;
+    }
+  ]);
+  
+
+  
+}
+
+function withIconBadgeAndroid(config: any, badges: Array<Badge>) {
+  return withDangerousMod(config, [
+    "android",
+    async (config) => {
+      // Update the main icon
+      config = await withIcon(config, badges)
+
+      const adaptiveIconPath = config?.android?.adaptiveIcon?.foregroundImage;
+      console.log("Android Icon path", adaptiveIconPath)
+
+      // Generate icon with badge
+      if (adaptiveIconPath) {
+        await addBadge({
+          icon: adaptiveIconPath,
+          dstPath: DST_ADAPTIVE_APP_ICON,
+          badges,
+        }).catch(() => {
+        }); // we silently fail to prevent error in android build process
+
+        config.android!.adaptiveIcon!.foregroundImage = DST_ADAPTIVE_APP_ICON;
+      }
+
+      return config;
+    },
+    
+  ]);
+}
+
 function withIconBadge(config: any, { badges, enabled = true }: Params) {
   if (!enabled) return config;
+
+  config = withIconBadgeAndroid(config, badges);
+  config = withIconBadgeiOS(config, badges);
+
+  return config;
+
   // get source paths from config
   const iconPath = config?.icon;
   const adaptiveIconPath = config?.android?.adaptiveIcon?.foregroundImage;

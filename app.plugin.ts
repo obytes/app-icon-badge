c0f7ffb58b96f5bdd9bfd35ee5d0f656958b809b
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2024-10-22 12:42:52 +0100

    feat: add typing for plugin functions

--- app.plugin.ts
+++ app.plugin.ts
@@ -1,96 +1,152 @@
 import { addBadge } from './index';
-import { Badge } from './types';
-import { withDangerousMod } from '@expo/config-plugins'
+import { AppIconBadgeOptions, Badge } from './types';
+import {
+  withDangerousMod,
+  ConfigPlugin,
+  ExportedConfigWithProps,
+} from '@expo/config-plugins';
 
 const DST_APP_ICON_BADGE_FOLDER = '.expo/app-icon-badge';
 const DST_ICON = `${DST_APP_ICON_BADGE_FOLDER}/icon.png`;
-const DST_IOS_ICON = `${DST_APP_ICON_BADGE_FOLDER}/ios-icon.png`;
 const DST_ADAPTIVE_APP_ICON = `${DST_APP_ICON_BADGE_FOLDER}/foregroundImage.png`;
 
-type Params = {
-  badges: Array<Badge>;
-  enabled?: boolean;
+interface BadgeResult {
+  error?: string;
+  status: 'success' | 'error';
+}
+
+/**
+ * A wrapper around addBadge that handles errors
+ * @param params - Parameters for the addBadge function
+ * @returns A promise that resolves to a BadgeResult object with status and error in case of error
+ */
+
+const addBadgeWithErrorsHandling = async (
+  params: Parameters<typeof addBadge>[0]
+): Promise<BadgeResult> => {
+  try {
+    await addBadge(params);
+    return { status: 'success' };
+  } catch (error) {
+    return { error: 'An unknown error occurred', status: 'error' };
+  }
 };
 
+/**
+ * In case the user has provided an icon, in the global config, we need to generate the icon with the badge and update the config
+ * @param config - Expo config
+ * @param badges - Array of badges to add
+ * @returns The updated config
+ */
 
-async function withIcon(config: any, badges: Array<Badge>) {
-  
+async function withIcon(
+  config: ExportedConfigWithProps<unknown>,
+  badges: Array<Badge>
+) {
   const iconPath = config?.icon;
-  
   if (iconPath) {
-    await addBadge({
+    const result = await addBadgeWithErrorsHandling({
       icon: iconPath,
       dstPath: DST_ICON,
       badges,
-    }).catch(() => {}); // we silently fail to prevent error in android build process
+    });
 
-    config.icon = DST_ICON;
+    if (result.status === 'success') {
+      config.icon = DST_ICON;
+    }
   }
-  return config
+  return config;
 }
 
-function withIconBadgeiOS(config: any, badges: Array<Badge>) {
+/**
+ * For iOS and icon inside ios config
+ * @param config - Expo config
+ * @param badges - Array of badges to add
+ * @returns The updated config
+ */
+
+const withIconBadgeiOS: ConfigPlugin<AppIconBadgeOptions> = (
+  config,
+  options
+) => {
   return withDangerousMod(config, [
-    "ios",
+    'ios',
     async (config) => {
-
-      // Update the main icon
-      config = await withIcon(config, badges)
-
+      const { badges = [] } = options;
+      config = await withIcon(config, badges);
       const iconPath = config?.ios?.icon;
-  
+
       if (iconPath) {
-        await addBadge({
+        const result = await addBadgeWithErrorsHandling({
           icon: iconPath,
           dstPath: DST_ICON,
           badges,
-        }).catch(() => {}); // we silently fail to prevent error in android build process
-    
-        config!.ios!.icon = DST_ICON;
-      }
+        });
 
-      return config;
-    }
-  ]);
-  
-
-  
-}
-
-function withIconBadgeAndroid(config: any, badges: Array<Badge>) {
-  return withDangerousMod(config, [
-    "android",
-    async (config) => {
-      // Update the main icon
-      config = await withIcon(config, badges)
-
-      const adaptiveIconPath = config?.android?.adaptiveIcon?.foregroundImage;
-      console.log("Android Icon path", adaptiveIconPath)
-
-      // Generate icon with badge
-      if (adaptiveIconPath) {
-        await addBadge({
-          icon: adaptiveIconPath,
-          dstPath: DST_ADAPTIVE_APP_ICON,
-          badges,
-        }).catch(() => {
-        }); // we silently fail to prevent error in android build process
-
-        config.android!.adaptiveIcon!.foregroundImage = DST_ADAPTIVE_APP_ICON;
+        if (result.status === 'success') {
+          config!.ios!.icon = DST_ICON;
+        }
       }
 
       return config;
     },
-    
   ]);
-}
+};
 
-function withIconBadge(config: any, { badges, enabled = true }: Params) {
+/**
+ * For Android and icon inside android config
+ * @param config - Expo config
+ * @param badges - Array of badges to add
+ * @returns The updated config
+ */
+
+const withIconBadgeAndroid: ConfigPlugin<AppIconBadgeOptions> = (
+  config,
+  options
+) => {
+  return withDangerousMod(config, [
+    'android',
+    async (config) => {
+      const { badges = [] } = options;
+      // Update the main icon
+      config = await withIcon(config, badges);
+
+      const adaptiveIconPath = config?.android?.adaptiveIcon?.foregroundImage;
+      console.log('Android Icon path', adaptiveIconPath);
+
+      // Generate icon with badge
+      if (adaptiveIconPath) {
+        const result = await addBadgeWithErrorsHandling({
+          icon: adaptiveIconPath,
+          dstPath: DST_ADAPTIVE_APP_ICON,
+          badges,
+        });
+        if (result.status === 'success') {
+          config.android!.adaptiveIcon!.foregroundImage = DST_ADAPTIVE_APP_ICON;
+        }
+      }
+
+      return config;
+    },
+  ]);
+};
+
+/**
+ * The main plugin that adds the badge to the icon and updates the config
+ * checks if the plugin is enabled and then calls the other plugins for iOS and Android
+ * @param config - Expo config
+ * @param options - Options for the plugin
+ * @returns The updated config
+ */
+
+const withIconBadge: ConfigPlugin<AppIconBadgeOptions> = (config, options) => {
+  const { badges = [], enabled = true } = options;
   if (!enabled) return config;
 
-  config = withIconBadgeAndroid(config, badges);
-  config = withIconBadgeiOS(config, badges);
+  config = withIconBadgeAndroid(config, { badges });
+  config = withIconBadgeiOS(config, { badges });
 
   return config;
-}
+};
+
 module.exports = withIconBadge;
